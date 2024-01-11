9bc04400bbe815824d77c269c8076df488be8c7f
rinku-kr <rk24736599@gmail.com>
2024-01-11 11:07:53 +0530

    Extracting dynamic params

--- app.js
+++ app.js
@@ -22,3 +22,5 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 app.listen(3000);
+
+// https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
